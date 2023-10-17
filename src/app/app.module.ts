c04bb4f5fea386168d6955e129abb07757a6e894
Domashnev <domashnev@gmail.com>
2023-10-17 11:34:37 +0300

    add firebase, bootsrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
+import { provideMessaging,getMessaging } from '@angular/fire/messaging';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    provideStorage(() => getStorage())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
+  firebase: {
+    projectId: 'scrabble-domik',
+    appId: '1:197864339915:web:f7abbb1cf2d94495e6de4e',
+    databaseURL: 'https://scrabble-domik.firebaseio.com',
+    storageBucket: 'scrabble-domik.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDh_HmO5I3TrNKMlLV5xxT0Dqr8YKc9FlY',
+    authDomain: 'scrabble-domik.firebaseapp.com',
+    messagingSenderId: '197864339915',
+    measurementId: 'G-KBMWWVQNBL',
+  },
   production: true
 };
